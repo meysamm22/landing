7f83bfa7bb2b9fe3a1cd65c0cd1b860058783ab1
Meysam Zarei <mey.zarei@yahoo.com>
2025-09-01 14:44:17 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: './',
+    base: '/build',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
